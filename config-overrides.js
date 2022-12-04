a7bd08b805eb0b86f7bd322a419e12dd8015a4f8
william2022pan <anger_god@163.com>
2022-12-04 16:03:52 +0800

    submit some modify for the project

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// // 项目根目录下，新增文件 config-overrides.js
+// const { override, fixBabelImports } = require('customize-cra');
+
+// module.exports = override(
+// fixBabelImports('import', {
+//   libraryName: 'antd',
+//   libraryDirectory: 'es',
+//   style: 'css',
+// }),
+// );
